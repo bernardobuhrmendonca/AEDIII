2820d7987a53bf2d22fe9d3934d01c31f7cfcad3
Berna <bbam2503@gmail.com>
2024-09-08 11:47:30 -0300

    Commentary addition

--- src/ex01/BinConversor.java
+++ src/ex01/BinConversor.java
@@ -11,8 +11,8 @@ public class BinConversor {
     public void setBin(int[] bin) {
         this.bin = bin;
     }
-    int setCaseBin(int casa) {
-        this.bin[casa];
+    public void setCaseBin(int casa, int value) {
+        this.bin[casa] = value;
     }
 
     public int[] getDec() {
@@ -23,13 +23,18 @@ public class BinConversor {
         this.dec = dec;
     }
 
+    //Used for convert a decimal number to a binary one.
     public int[] Converse(int[] vec) {
+        //This loop is used for covert all items in an vector and tranffer to another one, dec to bin.
         for (int j = 0; j < vec.length; j++) {
             bin[j] = 1;
+            //if the dec number is equal to 0, the return in bin vector will be 0 too.
             if (dec[j] == 0) {
                 bin[j] = 0;
             }
+            //for the cases that the number isn't 0.
             while (dec[j] != 0) {
+                //Implementing the sucessive division per two conversion method.
                 if (dec[j] / 2 != 0) {
                     bin[j] = bin[j] * 10;
                     dec[j] = dec[j] / 2;
@@ -39,6 +44,7 @@ public class BinConversor {
                 }
             }
         }
+        //Returning the vector with the results of the process.
         return bin;
     }
 }
